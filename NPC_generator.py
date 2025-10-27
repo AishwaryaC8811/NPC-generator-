5fe0b81c4639c45d9ebbcc5bb2bf4bf4c1b4e437
Aishwarya C <aishwaryachandalada@gmail.com>
2025-10-27 17:25:22 +0000

    npc generator

---
+++ NPC_generator.py
@@ -0,0 +1,51 @@
+npc_names = input("List ten names: ")
+
+npc_list = []
+npc_list.append(npc_names)
+# print(npc_list)
+
+import random
+
+attributes = [
+    "brave",
+    "loyal",
+    "curious",
+    "wise",
+    "kind",
+    "honest",
+    "clever",
+    "bold",
+    "gentle",
+    "cunning",
+    "patient",
+    "fearless",
+    "stubborn",
+    "generous",
+    "ruthless",
+    "calm",
+    "charming",
+    "proud",
+    "humble",
+    "caring",
+    "mysterious",
+    "stern",
+    "witty",
+    "thoughtful",
+    "reckless",
+    "focused",
+    "cheerful",
+    "vengeful",
+    "serious",
+    "forgiving",
+    "confident",
+    "quiet",
+    "daring",
+    "shy",
+    "friendly",
+    "jealous",
+    "noble",
+    "cruel",
+    "hopeful",
+    "ambitious"
+]
+
